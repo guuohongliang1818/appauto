eb63ebd5861794ac81ca5ca18e3c17799a725e02
guohongliang <guohongliang@163.com>
2023-05-13 15:22:51 +0800

    手写web自动化case2

---
+++ tests/page/main_page.py
@@ -0,0 +1,11 @@
+# 姓名：郭宏亮
+# 时间：2023/5/13 15:15
+# 每一个页面对应一个page
+# 每个页面中的功能（单击事件等），对应一个方法
+class MainPage:
+    def get_topic_list(self):
+        pass
+
+    def to_search_advance(self):
+        # 调到搜索页面
+        pass
---
+++ tests/page/search_page.py
@@ -0,0 +1,6 @@
+# 姓名：郭宏亮
+# 时间：2023/5/13 15:19
+class SearchPage:
+
+    def search(self, keyword):
+        pass
