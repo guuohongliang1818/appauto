b763435da21d6eb2c97992914e370d251ec794c1
guohongliang <guohongliang@163.com>
2023-08-16 21:56:59 +0800

    window打开关闭测试

--- test_dushuwu/class04/test_window.py
+++ test_dushuwu/class04/test_window.py
@@ -23,6 +23,52 @@ def test_confirm():
     print(driver.window_handles)
     assert len(driver.window_handles) == 1
 
+    # 点击作家专栏
+    driver.find_element(By.XPATH, "//div[@id='mainNav']//a[text()='作家专区']").click()
+    # 等待新窗口或标签页被打开
+    WebDriverWait(driver, 5).until(expected_conditions.number_of_windows_to_be(2))
+    sleep(2)
+    # 获取所有的窗口页签
+    # new_hd = driver.current_window_handle
+    # print("当前窗口的句柄id", new_hd)
+    print(driver.window_handles)
+
+    # 切换窗口，方式1：循环执行，知道找到新的窗口句柄
+    '''
+    for hd in driver.window_handles:
+        if hd != original_hd:
+            driver.switch_to.window(hd)
+            break
+    print("当前窗口的句柄id", driver.current_window_handle)
+    '''
+    # 切换窗口，方式1：切换到新打开的窗口
+    # n=1，原始的是0，一次类推
+    # n=-1，代表最后一个打开的页签
+    driver.switch_to.window(driver.window_handles[-1])
+    print("当前窗口的句柄id", driver.current_window_handle)
+
+    # 等待新页签完成加载内容，通过标题
+    WebDriverWait(driver, 5).until(expected_conditions.title_is("会员登录_读书屋"))
+    # 进行登录操作
+    username = "18899991234"
+    passwd = "123456"
+
+    driver.find_element(By.XPATH, "//a[text()='登录']").click()
+
+    driver.find_element(By.ID, "txtUName").send_keys(username)
+    driver.find_element(By.ID, "txtPassword").send_keys(passwd)
+    driver.find_element(By.ID, "btnLogin").click()
+    sleep(2)
+    # 关闭当前页签和窗口
+    driver.close()
+    # driver.quit()退出浏览器
+
+    # 切回到原始的窗口完成刷新页面，完成加载内容
+    driver.switch_to.window(original_hd)
+    sleep(2)
+    driver.refresh()
+    sleep(2)
+
 
 if __name__ == '__main__':
     pytest.main(['-v'])
